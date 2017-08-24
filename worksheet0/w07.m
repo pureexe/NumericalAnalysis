e22e5b4b99d83aad5d20f255ff2bc329118b9b64
Pakkapon Phongthawee <me@pureapp.in.th>
2017-08-24 12:06:39 +0700

    complete

---
+++ worksheet0/w07.m
@@ -0,0 +1,9 @@
+format long;
+f = @(y,x) [abs(y - x),abs(y - x)/x]
+x = 0.96240542;
+disp(f(1.72645302,x))
+disp(f(1.34979704,x))
+disp(f(1.11098136,x))
+disp(f(0.99418235,x))
+disp(f(0.96426973,x))
+disp(f(0.96240541,x))
