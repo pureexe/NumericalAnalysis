cb693aa50f84f52f13902a52b04f30fe735cba09
Pakkapon Phongthawee <me@pureapp.in.th>
2017-11-09 11:58:18 +0700

    update: find

--- Experiment02/TableRender.m
+++ Experiment02/TableRender.m
@@ -5,6 +5,7 @@ b = 1;
 n = 10;
 h = (b-a)/n;
 max_error = [0, 2, 6, 12]; % manule find max|f''(c)|
+max_error_simpson = [0, 0, 0, 24]; % manule find max|f''''(c)|
 %% function creation
 for i = 1:4
     f{i} = @(x) x.^(i);
@@ -17,13 +18,13 @@ disp("  Exact    Trapzoid   Simpson");
 for i = 1:4
     exact(i) = integral(f{i},a,b);
     trapzoid(i) = TrapezoidIntegration(f{i},a,b,n);
-    simpson(i) = SimpsonIntegration(f{i},a,b,100);
+    simpson(i) = SimpsonIntegration(f{i},a,b,5);
     fprintf("%4.6f   %4.6f   %4.6f\n",exact(i),trapzoid(i),simpson(i));
 end
 %% render table 6
 disp("=======    TABLE 6    =======");
 trapzoid_error_expected = trapzoid_error(max_error);
-simpson_error_expected = simpson_error(max_error);
+simpson_error_expected = simpson_error(max_error_simpson);
 disp("Expected Trapzoid      Observed Trapzoid      Expected Simpson    Observed Simpson");
 for i = 1:4
     fprintf("    %4.6f               %4.6f              %4.6f             %4.6f\n",...
